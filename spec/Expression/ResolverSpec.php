f8aed23028455d2d924cc0bc3b28584b4bf7701d
Andrey Astakhov <andrey.astakhov@gmail.com>
2019-06-01 22:12:58 +0200

    Implement expression resolver.

--- spec/Expression/ResolverSpec.php
+++ spec/Expression/ResolverSpec.php
@@ -4,15 +4,34 @@ namespace spec\Aa\AkeneoFixtureLoader\Expression;
 
 use Aa\AkeneoFixtureLoader\Faker\FakerAdapter;
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
 
 class ResolverSpec extends ObjectBehavior
 {
-    function it_resolves(FakerAdapter $faker)
+    function let(FakerAdapter $faker)
     {
         $this->beConstructedWith($faker);
+    }
 
+    function it_resolves_a_simple_expression(FakerAdapter $faker)
+    {
         $faker->fake('expression', [])->shouldBeCalled();
 
-        $this->resolve('expression');
+        $this->resolve('< expression() >');
+    }
+
+    function it_resolves_a_complex_expression(FakerAdapter $faker)
+    {
+        $faker->fake('expr1', ['textParam', 120])->shouldBeCalled();
+        $faker->fake('expr2', [800, 'text'])->shouldBeCalled();
+
+        $this->resolve('First expression  one < expr1("textParam", 120) >, then expression 2 <expr2(800, "text")>, that\'s all');
+    }
+
+    function it_does_not_modify_a_text_if_it_is_not_an_expression(FakerAdapter $faker)
+    {
+        $faker->fake(Argument::type('string'), Argument::type('array'))->shouldNotBeCalled();
+
+        $this->resolve('it is not an expression');
     }
 }
--- src/Expression/Resolver.php
+++ src/Expression/Resolver.php
@@ -18,6 +18,28 @@ class Resolver
 
     public function resolve(string $expression)
     {
-        return $this->faker->fake($expression, []);
+        return preg_replace_callback('/\<\s?(\w+)\(([^\)]*)\)\s?\>/', [$this, 'fake'], $expression);
+    }
+
+    private function fake(array $matches)
+    {
+        $name = $matches[1];
+
+        $arguments = $this->parseArguments($matches[2]);
+
+        return $this->faker->fake($name, $arguments);
+    }
+
+    private function parseArguments(string $argumentText): array
+    {
+        if ('' === trim($argumentText)) {
+            return [];
+        }
+
+        return array_map(function(string $text) {
+
+            return trim(trim($text), '"\'');
+
+        }, explode(',', $argumentText));
     }
 }
